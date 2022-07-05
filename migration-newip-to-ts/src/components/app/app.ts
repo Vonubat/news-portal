ae4ad83e535599cd87d0d2945ca0a69d8c5074dd
Egor Tabunov <Vonubat@gmail.com>
2022-07-05 20:35:26 +0300

    feat: add search functionality

--- migration-newip-to-ts/src/components/app/app.ts
+++ migration-newip-to-ts/src/components/app/app.ts
@@ -5,9 +5,11 @@ import { IGetArticles, IGetSources } from '../../types/index';
 class App {
     private controller: AppController;
     private view: AppView;
+    public SEARCH_INPUT: HTMLInputElement;
     public constructor() {
         this.controller = new AppController();
         this.view = new AppView();
+        this.SEARCH_INPUT = document.querySelector('#search') as HTMLInputElement;
     }
 
     public start(): void {
@@ -15,6 +17,34 @@ class App {
             this.controller.getNews(e, (data: IGetArticles): void => this.view.drawNews(data))
         );
         this.controller.getSources((data: IGetSources): void => this.view.drawSources(data));
+    }
+
+    public search(): void {
+        // realization of search engine
+        this.SEARCH_INPUT.addEventListener('keydown', function (e: KeyboardEvent): false | undefined {
+            if (e.key === 'Enter') {
+                e.preventDefault();
+                return false;
+            }
+        });
+
+        this.SEARCH_INPUT.addEventListener('keyup', function (e: KeyboardEvent): void {
+            e.key === 'Enter' ? e.preventDefault() : 42;
+            console.log(e);
+
+            // e.preventDefault();
+            const SOURCE_ITEMS = document.querySelectorAll('.source__item') as NodeListOf<HTMLDivElement>;
+
+            for (let i = 0; i < SOURCE_ITEMS.length; i++) {
+                const searchValue = new RegExp(`${this.value.toLowerCase()}`);
+                const itemValue: string = SOURCE_ITEMS[i].innerText.toLowerCase();
+                if (!itemValue.match(searchValue)) {
+                    SOURCE_ITEMS[i].style.display = 'none';
+                } else {
+                    SOURCE_ITEMS[i].style.display = 'block';
+                }
+            }
+        });
     }
 }
 
--- migration-newip-to-ts/src/index.ts
+++ migration-newip-to-ts/src/index.ts
@@ -1,11 +1,7 @@
 import App from './components/app/app';
 import './global.css';
-const app: App = new App();
-app.start();
 
-document.addEventListener('keypress', function (e) {
-    if (e.keyCode === 13 || e.which === 13) {
-        e.preventDefault();
-        return false;
-    }
-});
+const app: App = new App();
+
+app.start();
+app.search();
